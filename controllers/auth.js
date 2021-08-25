e6ca3e321f3c438bbd533abbbf86ac05d3c7f824
znapci <znapci@gmail.com>
2021-08-25 14:54:59 +0000

    Init auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,12 @@
 const jwt = require('jsonwebtoken')
+const userdb = require('../data/users.json')
 
-const Auth = (req,res,next)=>{
-    const token = req.headers.
-    jwt.verify()
+const Auth = (req, res, next) => {
+    const token = req.headers.authorization
+    const secret = process.env.AUTH_TOKEN_SECRET
+    const t = jwt.verify(token, secret)
+    console.log(t)
+    next()
 }
+
+exports.auth = Auth
