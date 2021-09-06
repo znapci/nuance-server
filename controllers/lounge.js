6c2608304019752c5973b9ef548565884ec79f67
znapci <znapci@gmail.com>
2021-09-06 17:31:09 +0530

    Wip

--- controllers/lounge.js
+++ controllers/lounge.js
@@ -1,22 +1,22 @@
 const Lounge = (req, res, next) => {
   if (req.user) {
-    const contacts = Array(25).fill({
-      id: 1,
-      peerId: '7d15b674b2cd8d2d05d8e327',
-      chatId: 1,
-      name: 'John Doe'
-    })
-    // const contacts = [{
+    // const contacts = Array(25).fill({
     //   id: 1,
-    //   peerId: '7d15b674b2cd8d2d05d8e327',
+    //   peerId: '594771cc81b7d63bf88fbf8ab5b55909ac0fc076478b83c090c10fa39991513d24e65e50',
     //   chatId: 1,
     //   name: 'John Doe'
-    // }, {
-    //   id: 2,
-    //   peerId: 'c7fd33a5dc425a9a7b959bfe9020670551c1ad5113cee0a8ac23a513f93e3cd897e86547',
-    //   chatId: 2,
-    //   name: `John's father`
-    // }]
+    // })
+    const contacts = [{
+      id: '613093cfa0d8deb19738bbd0',
+      peerId: '5c5b3efe322d409ba694dd1fd7641f63baf6e5b6b31066b0a53d42b2588e6efa2f7cba48',
+      chatId: 1,
+      name: 'John Doe'
+    }, {
+      id: '6130909c88b0d52426895de1',
+      peerId: 'ae1918e0e2154d05b0315678a0a0ab303a296cc6d328e1fef77dcfcb6159eee7c17bd0d5',
+      chatId: 2,
+      name: `John's father`
+    }]
     res.json({
       contacts
     })
@@ -29,7 +29,7 @@ const getChats = (req, res, next) => {
     res.json({
       chats: [{
         sender: '6130909c88b0d52426895de1',
-        reciever: 'Him',
+        reciever: '613093cfa0d8deb19738bbd0',
         content: 'Hiii',
         time: '1'
       }, {
