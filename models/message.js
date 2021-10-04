067b6484f4e211bfa04a7b9be2570ece6e711d52
znapci <znapci@gmail.com>
2021-10-04 21:20:26 +0530

    Many improvements
    -> Add serverside id generation
    -> Implement message delivery functions
    -> Change message structure to indicate message status
    -> Decommision temporary messageQueue and recieverQueue

--- models/message.js
+++ models/message.js
@@ -1,15 +1,27 @@
 const getDB = require('../util/db').getDB
 class Message {
-  constructor (sender, reciever, content) {
+  constructor (_id, sender, reciever, content, status) {
+    this._id = _id
     this.sender = sender
     this.reciever = reciever
     this.time = Date.now()
     this.content = content
+    this.status = status
   }
 
   save () {
     const db = getDB()
     return db.collection('messages').insertOne(this)
+  }
+
+  updateStatus (_id, status) {
+    const db = getDB()
+    return db.collection('messages').updateOne({
+      _id: { $eq: _id }
+    }, {
+      $set: { status: status }
+    }
+    )
   }
 }
 
--- socket.js
+++ socket.js
@@ -13,6 +13,8 @@ const sockets = (server) => {
   })
 
   io.use(socketsAuth)
+  // these are dummy as
+  // TODO: use db to check if user has pending messages
   const messageQueue = []
   const recieverQueue = []
   io.on('connection', socket => {
@@ -50,30 +52,42 @@ const sockets = (server) => {
     //     socket.to(chatId).emit('chatMessage', data)
     //   })
     // })
-    socket.on('chatMessage', data => {
-      socket.emit('messageDelivery', {
+    socket.on('chatMessage', (data, sendAck) => {
+      data.mId = Math.ceil(Math.random() * 1000000000000)
+      data.status = 1
+      sendAck({
         mId: data.mId,
-        status: 1
+        status: data.status
       })
       user.getSocketId(data.reciever).then(({ socketId }) => {
-        if (socketId === '') {
-          console.log('User offline adding to queue')
-          messageQueue.push(data)
-          if (recieverQueue.indexOf(data.reciever) === -1) { recieverQueue.push(data.reciever) }
-        } else {
-          console.log('Sending ', data.content, 'to', socketId)
-          const message = new Message(data.sender, data.reciever, data.content)
-          message.save().then(() => {
-            socket.emit('messageDelivery', {
-              mId: data.mId,
-              status: 2
-            })
-            socket.to(socketId).emit('chatMessage', data)
-          }).catch(err => console.err(err))
-        }
+        // if (socketId === '') {
+        //   console.log('User offline adding to queue')
+        //   //messageQueue.push(data)
+        //   //if (recieverQueue.indexOf(data.reciever) === -1) { recieverQueue.push(data.reciever) }
+        // }
+        console.log('Sending ', data.content, 'to', socketId)
+        const message = new Message(data.mId, data.sender, data.reciever, data.content, data.status)
+        message.save().then(() => {
+          socket.to(socketId).emit('chatMessage', data)
+        }).catch(err => console.error(err))
       }).catch(err => {
         console.error(err)
       })
+    })
+    socket.on('deliveryReport', (data) => {
+      console.log(data)
+      if (data.status === 2) {
+        const message = new Message()
+        const user = new User()
+        console.log('updating status')
+        message.updateStatus(data.mId, 2).then().catch(err => console.error(err))
+        user.getSocketId(data.sender).then(({ socketId }) => {
+          socket.to(socketId).emit('messageDelivery', {
+            mId: data.mId,
+            status: 2
+          })
+        })
+      }
     })
     socket.on('disconnect', () => {
       user.setSocketId('', socket.userId)
