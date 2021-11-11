d2e7e5f0edfdfa79d18e7f1abb033228d1789803
znapci <znapci@gmail.com>
2021-11-11 13:38:40 +0530

    Fix auth issue caused by 34e2eb27800270f7af97b5933070885c929e175c

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,20 +9,19 @@ const Auth = (req, res, next) => {
     if (decoded) {
       req.user = decoded.userId
       const user = new User()
-      next()
       // create a hash of jwt given to the client to compare
       // against an array of hashes in user database
-      // const hash = createHash('sha1').update(token).digest('base64')
-      // user.getSessions(decoded.userId).then(
-      //   result => {
-      //     if (result && result.sessions.includes(hash)) {
-      //       req.user = decoded.userId
-      //       next()
-      //     } else {
-      //       res.status(401).send('Token expired')
-      //     }
-      //   }
-      // ).catch(err => console.error(err))
+      const hash = createHash('sha1').update(token).digest('base64')
+      user.getSessions(decoded.userId).then(
+        result => {
+          if (result && result.sessions.includes(hash)) {
+            req.user = decoded.userId
+            next()
+          } else {
+            res.status(401).send('Token expired')
+          }
+        }
+      ).catch(err => console.error(err))
     } else {
       console.error(err)
       res.status(401).send('Token expired')

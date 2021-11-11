znapci/nuance-server

commit 34e2eb27800270f7af97b5933070885c929e175c

Changes in auth regarding 3a6262dc0617e3704d523aadea54d9a3ef79e558

controllers/auth.js
@@ -7,20 +7,22 @@ const Auth = (req, res, next) => {
   const secret = process.env.AUTH_TOKEN_SECRET
   jwt.verify(token, secret, (err, decoded) => {
     if (decoded) {
+      req.user = decoded.userId
       const user = new User()
+      next()
       // create a hash of jwt given to the client to compare
       // against an array of hashes in user database
-      const hash = createHash('sha1').update(token).digest('base64')
-      user.getSessions(decoded.userId).then(
-        result => {
-          if (result && result.sessions.includes(hash)) {
-            req.user = decoded.userId
-            next()
-          } else {
-            res.status(401).send('Token expired')
-          }
-        }
-      ).catch(err => console.error(err))
+      // const hash = createHash('sha1').update(token).digest('base64')
+      // user.getSessions(decoded.userId).then(
+      //   result => {
+      //     if (result && result.sessions.includes(hash)) {
+      //       req.user = decoded.userId
+      //       next()
+      //     } else {
+      //       res.status(401).send('Token expired')
+      //     }
+      //   }
+      // ).catch(err => console.error(err))
     } else {
       console.error(err)
       res.status(401).send('Token expired')

controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
-const { createHash } = require('crypto')
+//const { createHash } = require('crypto')
 
 const Login = (req, res, next) => {
   const tokenSecret = process.env.AUTH_TOKEN_SECRET
@@ -13,8 +13,8 @@ const Login = (req, res, next) => {
           const token = jwt.sign({
             userId: userCreds._id
           }, tokenSecret, { expiresIn: '100d' })
-          const sessionId = createHash('sha1').update(token).digest('base64')
-          user.addSession(sessionId, userCreds.sessions)
+          //const sessionId = createHash('sha1').update(token).digest('base64')
+          //user.addSession(sessionId, userCreds.sessions)
           res.json({
             id: userCreds._id,
             token

commit d2e7e5f0edfdfa79d18e7f1abb033228d1789803

Fix auth issue caused by 34e2eb27800270f7af97b5933070885c929e175c

controllers/auth.js
@@ -9,20 +9,19 @@ const Auth = (req, res, next) => {
     if (decoded) {
       req.user = decoded.userId
       const user = new User()
-      next()
       // create a hash of jwt given to the client to compare
       // against an array of hashes in user database
-      // const hash = createHash('sha1').update(token).digest('base64')
-      // user.getSessions(decoded.userId).then(
-      //   result => {
-      //     if (result && result.sessions.includes(hash)) {
-      //       req.user = decoded.userId
-      //       next()
-      //     } else {
-      //       res.status(401).send('Token expired')
-      //     }
-      //   }
-      // ).catch(err => console.error(err))
+      const hash = createHash('sha1').update(token).digest('base64')
+      user.getSessions(decoded.userId).then(
+        result => {
+          if (result && result.sessions.includes(hash)) {
+            req.user = decoded.userId
+            next()
+          } else {
+            res.status(401).send('Token expired')
+          }
+        }
+      ).catch(err => console.error(err))
     } else {
       console.error(err)
       res.status(401).send('Token expired')

controllers/lounge.js
@@ -17,38 +17,19 @@ const Lounge = (req, res, next) => {
     // })
     const contacts = [
       {
-        id: '613626668852483c04e43285',
-        chats: [{
-          sender: '6130909c88b0d52426895de1',
-          reciever: '613626668852483c04e43285',
-          content: 'Hiii',
-          time: '1'
-        }, {
-          sender: '2',
-          reciever: 'You',
-          content: 'Hello',
-          time: '2'
-        }
+        id: '618cc7f07b6f5d18b38a75db',
+        chats: [
         ],
         name: 'John Doe'
       },
       {
         id: '6136267d8852483c04e43286',
-        chats: [{
-          sender: '6130909c88b0d52426895de1',
-          reciever: '613626668852483c04e43285',
-          content: 'Hiii',
-          time: '1'
-        }, {
-          sender: '2',
-          reciever: 'You',
-          content: 'Hello',
-          time: '2'
-        }
+        chats: [
         ],
         name: 'Johnnny'
       }]
     const loungeUser = new ChatUser(req.user, true, contacts)
+    loungeUser.save()
     loungeUser.getContacts().then(contacts => {
       console.log(contacts)
       res.json(contacts)

controllers/sockets.js
@@ -11,23 +11,22 @@ const Auth = (socket, next) => {
     if (decoded) {
       console.log(decoded.userId + ' Connected')
       socket.userId = decoded.userId
-      return next()
-      // const user = new User()
+      const user = new User()
       // due to new direction of the project multilogin support is disabled indefinately
       // create a hash of jwt given to the client to compare
       // against an array of hashes in user database
-      // const hash = createHash('sha1').update(token).digest('base64')
-      // user.getSessions(decoded.userId).then(
-      //   result => {
-      //     if (result && result.sessions.includes(hash)) {
-      //       console.log('req authed')
-      //       socket.userId = decoded.userId
-      //       return next()
-      //     } else {
-      //       return next(new Error('Unauthorized'))
-      //     }
-      //   }
-      // ).catch(err => console.error(err))
+      const hash = createHash('sha1').update(token).digest('base64')
+      user.getSessions(decoded.userId).then(
+        result => {
+          if (result && result.sessions.includes(hash)) {
+            console.log('req authed')
+            socket.userId = decoded.userId
+            return next()
+          } else {
+            return next(new Error('Unauthorized'))
+          }
+        }
+      ).catch(err => console.error(err))
     } else {
       console.error(err)
       return next(new Error('Unauthorized'))

controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
-//const { createHash } = require('crypto')
+const { createHash } = require('crypto')
 
 const Login = (req, res, next) => {
   const tokenSecret = process.env.AUTH_TOKEN_SECRET
@@ -13,8 +13,8 @@ const Login = (req, res, next) => {
           const token = jwt.sign({
             userId: userCreds._id
           }, tokenSecret, { expiresIn: '100d' })
-          //const sessionId = createHash('sha1').update(token).digest('base64')
-          //user.addSession(sessionId, userCreds.sessions)
+          const sessionId = createHash('sha1').update(token).digest('base64')
+          user.addSession(sessionId, userCreds.sessions).then().catch(err => console.error(err))
           res.json({
             id: userCreds._id,
             token
@@ -24,7 +24,7 @@ const Login = (req, res, next) => {
         }
       }).catch(err => console.log(err))
     } else {
-      res.status(404).send('Invalid username or password!')
+      res.status(401).send('Invalid username or password!')
     }
   }).catch(err => console.log(err))
   // const password = req.body.password